81de0a2b5908f48a744f9587ce8904fa1a7de846
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-15 10:05:34 -0700

    feat(stripUnnecessary): add ability to remove overzealous annotation
    In some situations expression statements are annotated before
    their roles in the overall logic are found. For example, the
    assignment expression `$scope.someFunction = function(){}`
    would previously be parsed with `$scope.someFunction` as an
    annotated statement. This causes a left hand assignment error.
    Similarly the call `console.log("something")` should be wrapped
    in one annotation. This feature adds support for both calls.

--- nope.js
+++ nope.js
@@ -6,9 +6,25 @@ module.exports = function (fn, error) {
   var src = body(fn);
   var fnParams = params(fn);
   var newSrc = falafel(src, function (node) {
-    if (node.type.indexOf('Expression') > -1) {
+
+    function stripUnnecessary(nodePart) {
+      var changeSrc = nodePart.source();
+      if(changeSrc.indexOf('____(') !== -1) {
+        changeSrc = changeSrc.replace('____(', '');
+        changeSrc = changeSrc.substring(0, changeSrc.length - 1);
+        nodePart.update(changeSrc);
+      }
+    }
+
+    if (node.type.indexOf('Expression') > -1 ) {
+      if(node.type.indexOf('Assignment') > -1) {
+        stripUnnecessary(node.left);
+      }
+      if(node.type.indexOf('Call') > -1) {
+        stripUnnecessary(node.callee);
+      }
       var nodeSrc = node.source();
-      if (!nodeSrc.match(/^____\(.+\);?$/)) {
+      if (!nodeSrc.match(/^____\([\s\S]+\);?$/)) {
         node.update('____(' + nodeSrc + ')');
       }
     }
@@ -17,3 +33,4 @@ module.exports = function (fn, error) {
   newSrc += '; function ____ (val) { if (val instanceof Element) { ' + error + ' } return val; };';
   return Function.apply(null, fnParams.concat(newSrc));
 }
+
--- test/nope.spec.js
+++ test/nope.spec.js
@@ -21,9 +21,32 @@ describe('nope', function () {
     spyOn(console, 'log');
     var consoleLogError = 'console.log("Error");';
     var niceTry = nope(function () {
-       new Element();
+      new Element();
     }, consoleLogError);
     niceTry();
     expect(console.log).toHaveBeenCalledWith('Error');
+  });
+
+  it('should work with functions with parameters', function() {
+    var parameter = [];
+    var niceTry = nope(function (parameter) {
+    });
+    expect(niceTry).not.toThrow();
+  });
+
+  it('should work with assignment expressions', function() {
+    var niceTry = nope(function ($scope) {
+      $scope.someFunction = function() {
+        console.log('some function called');
+      };
+    });
+    expect(niceTry.toString().indexOf('____($scope.someFunction = ____(')).not.toEqual(-1);
+  });
+
+  it('should work with named functions', function() {
+    expect(function() {
+      var niceTry = nope(function namedFunction($scope) {
+      });
+    }).not.toThrow();
   });
 });
